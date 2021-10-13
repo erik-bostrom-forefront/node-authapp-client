05fcfde1524755850bb41b139ba32440cf7d06b3
Erik Boström <erik.bostrom@forefront.se>
2021-10-13 13:45:18 +0200

    connect login endpoint to client app

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,13 +3,48 @@ import {
     TextField,
     Button,
     Box,
-    Typography
+    Typography,
+    Grid
 } from '@mui/material';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 function Login () {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const email = data.get('email');
+        const password = data.get('password');
+
+        if(!(email, password)) {
+            alert('You must provide email and password');
+        }
+        const payload = {
+            email,
+            password
+        }
+
+        try {
+            const resp = await axios.post(process.env.REACT_APP_AUTH_URL + 'users/login', payload);
+            if (resp.status === 200) {
+                alert('User logged in');
+                console.log(resp.data);
+                const user = jwt_decode(resp.data.token);
+                console.log(user);
+            }
+        } catch (err) {
+            console.error(err);
+            alert('no login for you');
+        }
+
+    }
     return (
-        <Box>
-        <div className="login">
+        <Box sx={{
+            marginTop:8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }}>
             <Typography variant='h3' component='div' sx={{
                     display: 'flex',
                     justifyContent: 'center',
@@ -17,10 +52,33 @@ function Login () {
                 }}>
                 Login
             </Typography>
-            <TextField id="login-email" label="Email" />
-            <TextField id="login-password" label="Password" type="password" />
-            <Button variant='contained'>Login</Button>
-        </div>
+        <Box component='form' noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <TextField 
+                        id='email'
+                        name='email'
+                        label='Email'
+                        fullWidth
+                        />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField 
+                        id='password'
+                        name='password'
+                        label='Password'
+                        type='password' 
+                        fullWidth
+                        />
+                </Grid>
+            </Grid>
+            <Button 
+                type='submit'
+                fullWidth
+                variant='contained'
+                sx={{mt:3, mb:2}}
+            >Login</Button>
+        </Box>
         </Box>
     );
 }
